78eb9770e3f3fbf924cd29bec839cd2f33430109
Pablo Castillo <pabloficial96@gmail.com>
2024-06-14 15:07:00 -0500

    new implementations with standard

--- ls-advanced.mjs
+++ ls-advanced.mjs
@@ -1,40 +1,38 @@
-import fs from 'node:fs/promises'
-import { join } from 'node:path'
-import pc from 'picocolors'
-
+import fs from "node:fs/promises";
+import { join } from "node:path";
+import pc from "picocolors";
 
 //funcion asincrona
 export async function ls(folder) {
-    let files
-   //try para el await
+  let files;
+  //try para el await
+  try {
+    files = await fs.readdir(folder);
+  } catch {
+    console.log(`Folder not found ${folder}`);
+    process.exit(1); //si hay algun error, sale en 1
+  }
+
+  const filePromises = files.map(async (file) => {
+    const filePath = join(folder, file);
+    let fileStats;
     try {
-        files = await fs.readdir(folder)
-    } catch {
-        console.log(`Folder not found ${folder}`)
-        process.exit(1) //si hay algun error, sale en 1
+      fileStats = await fs.stat(filePath);
+    } catch (err) {
+      console.error(pc.red(`Error reading the file ${filePath} ${err}`));
+      process.exit(1); //si hay algun error, sale en 1
     }
 
-    const filePromises = files.map(async file => {
-        const filePath = join(folder, file)
-        let fileStats
-        try {
-            fileStats = await fs.stat(filePath)
-        } catch(err) {
-            console.error(pc.red(`Error reading the file ${filePath} ${err}`))
-            process.exit(1) //si hay algun error, sale en 1
-        }
+    const isDirectory = fileStats.isDirectory();
+    const fileType = isDirectory ? "d" : "f";
+    const fileSize = fileStats.size;
+    const fileModified = fileStats.mtime.toLocaleString();
 
-        const isDirectory   = fileStats.isDirectory()
-        const fileType     = isDirectory ? 'd' : 'f'
-        const fileSize      = fileStats.size
-        const fileModified  = fileStats.mtime.toLocaleString()
+    return `${fileType} ${pc.blue(file.padEnd(20))} ${pc.green(
+      fileSize.toString().padStart(10).padEnd(20)
+    )} ${pc.yellow(fileModified.padStart(10))}`;
+  });
 
-
-        return `${fileType} ${pc.blue(file.padEnd(20))} ${pc.green(fileSize.toString().padStart(10).padEnd(20))} ${pc.yellow(fileModified.padStart(10))}`
-    }
-    )
-
-    const filesInfo = await Promise.all(filePromises)
-    filesInfo.forEach(fileInfo => console.log(fileInfo))
+  const filesInfo = await Promise.all(filePromises);
+  filesInfo.forEach((fileInfo) => console.log(fileInfo));
 }
-
